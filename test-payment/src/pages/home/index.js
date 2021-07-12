013e36025941eb17a6785291c29132ad5cccda23
Kien Nguyen <kien.nguyen3@tiki.vn>
2021-07-12 16:38:53 +0700

    step5: using tnx library

--- test-payment/src/pages/home/index.js
+++ test-payment/src/pages/home/index.js
@@ -1,24 +1,36 @@
 import { BASE_URL } from '../../utils/config';
-import myx from '../../utils/myx';
+import myx from 'tiki-miniapp-tnx/src/myx';
 
 Page({
   async onTapLogin() {
-    const info = await myx.getAuthCode()
-    const res = await myx.request({
-      url: `${BASE_URL}/oauth/token`,
-      headers: {
-        'UserAgent': 'Tini App Test Payment',
-        'Content-type': 'application/json'
-      },
-      method: 'POST',
-      data: {
-        auth_code: info.authCode,
-      },
+    this.setData({
+      loading: true
     })
-    console.log({res})
-    my.alert({
-      title: 'Thông báo',
-      content: 'my.request success'
-    })
+
+    try {
+      const info = await myx.getAuthCode()
+      const res = await myx.request({
+        url: `${BASE_URL}/oauth/token`,
+        headers: {
+          'UserAgent': 'Tini App Test Payment',
+          'Content-type': 'application/json'
+        },
+        method: 'POST',
+        data: {
+          auth_code: info.authCode,
+        },
+      })
+      myx.debug(`login successful`)
+    } finally {
+      this.setData({
+        loading: false
+      })
+    }
   },
+
+  onGoToDebug() {
+    my.navigateTo({
+      url: '/pages/debug/index'
+    })
+  }
 });
