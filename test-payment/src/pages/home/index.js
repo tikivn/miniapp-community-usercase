698a49103603a78693fe46a7e7681cacb93b1e86
Kien Nguyen <kien.nguyen3@tiki.vn>
2021-07-12 16:20:33 +0700

    step3: refactor my api to async version

--- test-payment/src/pages/home/index.js
+++ test-payment/src/pages/home/index.js
@@ -1,40 +1,42 @@
 import { BASE_URL } from '../../utils/config';
 
-Page({
-  onTapLogin() {
+async function getAuthCodeAsync() {
+  return new Promise((resolve, reject) => {
     my.getAuthCode({
-      success: (info) => {
-        my.request({
-          url: `${BASE_URL}/oauth/token`,
-          headers: {
-            'UserAgent': 'Tini App Test Payment',
-            'Content-type': 'application/json'
-          },
-          method: 'POST',
-          data: {
-            auth_code: info.authCode,
-          },
-          success: (res) => {
-            console.log({res})
-            my.alert({
-              title: 'Thông báo',
-              content: 'my.request success'
-            })
-          },
-          fail: (err) => {
-            my.alert({
-              title: 'Thông báo',
-              content: 'my.request fail error'
-            })
-          }
-        })
+      success: (info) => resolve(info),
+      fail: (err) => reject(err),
+    })
+  })
+}
+
+async function requestAsync(opts) {
+  return new Promise((resolve, reject) => {
+    my.request({
+      ...opts,
+      success: (res) => resolve(res),
+      fail: (err) => reject(err),
+    })
+  })
+}
+
+Page({
+  async onTapLogin() {
+    const info = await getAuthCodeAsync()
+    const res = await requestAsync({
+      url: `${BASE_URL}/oauth/token`,
+      headers: {
+        'UserAgent': 'Tini App Test Payment',
+        'Content-type': 'application/json'
       },
-      fail: (error) => {
-        my.alert({
-          title: 'Thông báo',
-          content: 'my.getAuthCode error'
-        })
-      }
+      method: 'POST',
+      data: {
+        auth_code: info.authCode,
+      },
+    })
+    console.log({res})
+    my.alert({
+      title: 'Thông báo',
+      content: 'my.request success'
     })
   },
 });
