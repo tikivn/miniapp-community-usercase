f38aeba9270d70543e8c175684fadc1df2303293
Kien Nguyen <kien.nguyen3@tiki.vn>
2021-07-12 16:06:44 +0700

    step2: exchange auth code with token

--- test-payment/src/pages/home/index.js
+++ test-payment/src/pages/home/index.js
@@ -1,10 +1,32 @@
+import { BASE_URL } from '../../utils/config';
+
 Page({
   onTapLogin() {
     my.getAuthCode({
       success: (info) => {
-        my.alert({
-          title: 'Thông báo',
-          content: 'my.getAuthCode success'
+        my.request({
+          url: `${BASE_URL}/oauth/token`,
+          headers: {
+            'UserAgent': 'Tini App Test Payment',
+            'Content-type': 'application/json'
+          },
+          method: 'POST',
+          data: {
+            auth_code: info.authCode,
+          },
+          success: (res) => {
+            console.log({res})
+            my.alert({
+              title: 'Thông báo',
+              content: 'my.request success'
+            })
+          },
+          fail: (err) => {
+            my.alert({
+              title: 'Thông báo',
+              content: 'my.request fail error'
+            })
+          }
         })
       },
       fail: (error) => {
