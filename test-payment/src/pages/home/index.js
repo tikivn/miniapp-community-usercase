8d60403da31053269d7d02757bbb690d66b4348c
Kien Nguyen <kien.nguyen3@tiki.vn>
2021-07-12 16:23:37 +0700

    step4: implement myx with proxy

--- test-payment/src/pages/home/index.js
+++ test-payment/src/pages/home/index.js
@@ -1,28 +1,10 @@
 import { BASE_URL } from '../../utils/config';
-
-async function getAuthCodeAsync() {
-  return new Promise((resolve, reject) => {
-    my.getAuthCode({
-      success: (info) => resolve(info),
-      fail: (err) => reject(err),
-    })
-  })
-}
-
-async function requestAsync(opts) {
-  return new Promise((resolve, reject) => {
-    my.request({
-      ...opts,
-      success: (res) => resolve(res),
-      fail: (err) => reject(err),
-    })
-  })
-}
+import myx from '../../utils/myx';
 
 Page({
   async onTapLogin() {
-    const info = await getAuthCodeAsync()
-    const res = await requestAsync({
+    const info = await myx.getAuthCode()
+    const res = await myx.request({
       url: `${BASE_URL}/oauth/token`,
       headers: {
         'UserAgent': 'Tini App Test Payment',
